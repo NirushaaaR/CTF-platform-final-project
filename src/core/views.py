6d85e5999732b60918e1f530ad22faf6d03339bb
NirushaaaR <nirush_srijaimoon@gmail.com>
2020-10-03 23:40:15 +0700

    add templates

--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("index page")
+    return render(request, "core/index.html")
