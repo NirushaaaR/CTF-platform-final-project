d23bea9514be8a83423fe65ad8df726ae74a0efc
NirushaaaR <nirush_srijaimoon@gmail.com>
2020-10-04 08:23:56 +0700

    fix model

--- src/core/models.py
+++ src/core/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 
 class Room(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    conclusion = models.TextField()
+    conclusion = models.TextField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    prerequisites = models.ManyToManyField("self", blank=True)
+    prerequisites = models.ManyToManyField("self", symmetrical=False, blank=True)
     participants = models.ManyToManyField(get_user_model(), through="UserParcitipation", blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class UserParcitipation(models.Model):
@@ -21,8 +25,12 @@ class UserParcitipation(models.Model):
     is_finish = models.BooleanField(default=False)
     finished_at = models.DateTimeField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.user} - {self.room}"
+
 
 class Task(models.Model):
+    task_number = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     title = models.CharField(max_length=255)
     description = models.TextField()
     flag = models.CharField(max_length=255)
@@ -32,13 +40,22 @@ class Task(models.Model):
         get_user_model(), through="UserAnsweredTask", blank=True
     )
 
+    def __str__(self):
+        return self.title
+
 
 class UserAnsweredTask(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     answered_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user} - {self.task}"
+
 
 class TaskHint(models.Model):
     hint = models.CharField(max_length=255)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hint
