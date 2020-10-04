c3d234d4d7826777a7fb9fda9eacd6b27d49e1e2
NirushaaaR <nirush_srijaimoon@gmail.com>
2020-10-04 09:22:03 +0700

    query rooms in index page

--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from .models import Room
+
 def index(request):
-    return render(request, "core/index.html")
+    """ The Fist page. Will Get Rooms and shows a paginated result """
+    rooms = Room.objects.all().order_by("created_at")
+    context = {
+        "rooms": rooms
+    }
+    return render(request, "core/index.html", context)
