3d0e2b8d8e35b83f39224136b2bcb05beca00ed4
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2020-11-05 17:34:07 +0000

    add AllowedHostsOriginValidator

--- src/app/asgi.py
+++ src/app/asgi.py
@@ -2,16 +2,17 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 import game.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
 
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            game.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(game.routing.websocket_urlpatterns)),
+        ),
+    }
+)
