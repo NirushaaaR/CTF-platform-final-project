b0c99e6aa0cfe2a6cfa3351a543eb30b21a87f25
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2020-11-08 21:39:59 +0700

    prepare game route

--- src/game/consumers.py
+++ src/game/consumers.py
@@ -1,45 +1,34 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.game_name = self.scope["url_route"]["kwargs"]["game_slug"]
+        self.game_group_name = "game_%s" % self.game_name
 
         # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.game_group_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.game_group_name, self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
+            self.game_group_name,
+            text_data_json,
         )
 
     # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
+    async def answer_flag(self, event):
+        flag = event["data"]["flag"]
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        # check flag
+        print(flag)
--- src/game/views.py
+++ src/game/views.py
@@ -2,8 +2,15 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from .models import Game
+
 def index(request):
-    return HttpResponse("Index")
+    games = Game.objects.all()
+    context = {"games": games}
+    print(games)
+    return render(request, "game/index.html", context)
 
 def game(request, game_slug):
-    return HttpResponse("game "+ game_slug)
+    game = Game.objects.prefetch_related("challenges").get(slug=game_slug)
+    context = {"game": game}
+    return render(request, "game/game.html", context)
