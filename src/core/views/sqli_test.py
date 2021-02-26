a54c9b7493227e8037a87cab45641b2806f44547
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2021-02-26 19:26:08 +0700

    add more sqli content

--- src/core/views/sqli_test.py
+++ src/core/views/sqli_test.py
@@ -9,9 +9,13 @@ def get_sql_response(request):
     username = request.POST.get('username')
     password = request.POST.get('password')
     conn = sqlite3.connect(str(settings.BASE_DIR / 'sqli.db'))
-
-    cursor = conn.execute(f"SELECT username, password, quote FROM Users WHERE username='{username}' and password='{password}'")
-    result = [{"username": row[0], "password": row[1], "quote": row[2]} for row in cursor]
-    conn.close()
+    result = None
+    try:
+        cursor = conn.execute(f"SELECT username, password, quote, 1, 2, 3 FROM Users WHERE username='{username}' and password='{password}'")
+        result = [{"username": row[0], "password": row[1], "quote": row[2]} for row in cursor]
+    except Exception as e:
+        result = "Error: "+str(e)
+    finally:
+        conn.close()
 
     return JsonResponse({"data": result})
